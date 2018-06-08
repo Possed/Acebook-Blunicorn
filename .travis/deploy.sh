df747e41cb1083b0f130a0f74f6ad4eff15ada84
George Sykes <georgesykes86@gmail.com>
2018-06-08 14:39:10 +0100

    One deploy shell script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    TAG="latest"
+else
+    TAG="$TRAVIS_BRANCH"
+fi
+docker build -t acebook-blunicorn .
+docker tag acebook-blunicorn georgesykes86/acebook-blunicorn:$TAG
+docker push georgesykes86/acebook-blunicorn:$TAG
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
 docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
