75c1c3033fe8ba976837990192f81500cabc43ec
George Sykes <georgesykes86@gmail.com>
2018-06-13 14:52:08 +0100

    Adds UserServiceImpl tests, complete

--- src/test/java/com/makersacademy/acebook/unit/UserServiceImplTest.java
+++ src/test/java/com/makersacademy/acebook/unit/UserServiceImplTest.java
@@ -1,24 +1,27 @@
 package com.makersacademy.acebook.unit;
 
 import com.makersacademy.acebook.User;
+import com.makersacademy.acebook.UserRegistrationDto;
 import com.makersacademy.acebook.UserRepository;
 import com.makersacademy.acebook.UserServiceImpl;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
-import javax.annotation.Resource;
+import org.junit.rules.ExpectedException;
+import org.mockito.*;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
 public class UserServiceImplTest {
 
 
-    private final String testEmail = "testEmail";
+    private final String testEmail = "test@email.com";
+    private final String testUsername = "username";
+    private final String testPassword = "password";
+    private final User testUser = new User(testUsername, testEmail, testPassword);
 
     @InjectMocks
     private UserServiceImpl userService;
@@ -26,26 +29,43 @@ public class UserServiceImplTest {
     @Mock
     UserRepository userRepository;
 
-    @Mock User user = new User("testuser", "test@emial.com", "password");
+    @Mock
+    private UserRegistrationDto userDto;
+
+    @Mock
+    private User user;
 
     @Before
     public void initMocks(){
         MockitoAnnotations.initMocks(this);
+        when(userRepository.findByEmail(testEmail)).thenReturn(testUser);
     }
-
-
 
     @Test
     public void findsByEmail() {
-        when(userRepository.findByEmail(testEmail)).thenReturn(user);
-
-        assertEquals(user, userService.findByEmail(testEmail));
-
+        assertEquals(testUser, userService.findByEmail(testEmail));
     }
 
     @Test
     public void savesUser() {
-        
+        userService.save(userDto);
+        verify(userRepository).save(Matchers.any(User.class));
+    }
+
+    @Test
+    public void loadsUserByUsername() {
+        when(userRepository.findByUsername(testUsername)).thenReturn(testUser);
+        assertTrue(userService.loadUserByUsername(testUsername) instanceof org.springframework.security.core.userdetails.User);
+    }
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void throwsErrorWhenUserWhenNotFound() {
+        when(userRepository.findByUsername(testUsername)).thenReturn(null);
+        exception.expect(UsernameNotFoundException.class);
+        userService.loadUserByUsername(testUsername);
     }
 
 
