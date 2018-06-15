635276eaf76dd63f8cbcc4d54ced270182a6b020
Muzzi Aldean <amiraldean@gmail.com>
2018-06-15 13:58:22 +0100

    basic like increment functionality and button

--- src/main/js/posts/post.js
+++ src/main/js/posts/post.js
@@ -2,16 +2,23 @@ import React from 'react';
 
 class Post extends React.Component{
 
-//    updateLikes() = {
-//        console.log("test");
-//    }
+    constructor(props) {
+        super(props);
+        this.state = this.props;
+        this.updateLikes = this.updateLikes.bind(this);
+    }
+
+    updateLikes() {
+        var thing = (this.state.post.likes +=1);
+        this.setState({likes: thing})
+    }
 
 	render() {
 		return [
 		    <div key="one" className='post-content'>{this.props.post.createdAt.split('T')[0]}</div>,
 		    <div key="two">{this.props.post.content}</div>,
 		    <span key="four">Likes: {this.props.post.likes}  </span>,
-		    <span key="five"><button><b>LIKE</b></button></span>,
+		    <span key="five"><button><b onClick={this.updateLikes}>LIKE</b></button></span>,
 		    <hr key="three" />,
         ];
 	}
